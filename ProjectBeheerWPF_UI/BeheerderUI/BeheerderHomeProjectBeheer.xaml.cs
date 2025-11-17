cb7d4e7704c475551b6e5c18cdd84c444282a54e
arnolambr <204155001+arnolambr@users.noreply.github.com>
2025-11-17 11:28:29 +0100

    home beheerders gemaakt

---
+++ ProjectBeheerWPF_UI/BeheerderUI/BeheerderHomeProjectBeheer.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectBeheerWPF_UI.BeheerderUI
+{
+    /// <summary>
+    /// Interaction logic for BeheerderHomeProjectBeheer.xaml
+    /// </summary>
+    public partial class BeheerderHomeProjectBeheer : Window
+    {
+        public BeheerderHomeProjectBeheer()
+        {
+            InitializeComponent();
+        }
+    }
+}
