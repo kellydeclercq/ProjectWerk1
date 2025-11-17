4b8b6ff03c8a0131978b69deb89c233ace280907
kellydeclercq <204192948+kellydeclercq@users.noreply.github.com>
2025-11-17 09:48:39 +0100

    BL opstart

---
+++ ProjectBeheerBL/Enumeraties/ProjectStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBeheerBL.Enumeraties
+{
+    internal class ProjectStatus
+    {
+    }
+}
