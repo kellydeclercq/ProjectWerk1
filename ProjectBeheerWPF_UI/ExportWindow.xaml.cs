73102e0b171356dcea53c86bfba28d8e7633e05d
JSTKaffie <haerincktom@proton.me>
2025-11-18 13:51:33 +0100

    exportwindow ui gemaakt

---
+++ ProjectBeheerWPF_UI/ExportWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectBeheerWPF_UI
+{
+    /// <summary>
+    /// Interaction logic for ExportWindow.xaml
+    /// </summary>
+    public partial class ExportWindow : Window
+    {
+        public ExportWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BladerenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExporteerAlsCSVButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExporteerAlsPDFButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
