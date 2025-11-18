beba33b578b4908b26adf4d3779459c203434d5a
JSTKaffie <haerincktom@proton.me>
2025-11-18 14:15:55 +0100

    managers imported, naam emailbox

--- ProjectBeheerWPF_UI/MainWindow.xaml.cs
+++ ProjectBeheerWPF_UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProjectBeheerBL.Beheerder;
+using ProjectBeheerBL.Domein;
+using ProjectBeheerDL_SQL;
+using ProjectBeheerUtils;
 
 namespace ProjectBeheerWPF_UI
 {
@@ -16,14 +21,40 @@ namespace ProjectBeheerWPF_UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ExportManager exportManager;
+        private GebruikersManager gebruikersManager;
+        private ProjectManager projectManager;
+        private BeheerMemoryFactory beheerMemoryFactory = new();
         public MainWindow()
         {
             InitializeComponent();
+
+            //nieuwe managers aanmaken
+            exportManager = new ExportManager();
+            gebruikersManager = new GebruikersManager(beheerMemoryFactory.GeefGebruikerRepo());
+            projectManager = new ProjectManager(beheerMemoryFactory.GeefProjectRepo());
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            Gebruiker gebruiker = gebruikersManager.GeefGebruikeradhvEmail(EmailTextBox.Text);
+            if (beh)
+            {
 
+                BeheerdersWindow beheerdersWindow = new BeheerdersWindow(lid, ledenManager, eventsManager, bestellingManager);
+                beheerdersWindow.Show();
+                this.Close();
+            }
+            else if (lid != null)
+            {
+                UserWindow userWindow = new UserWindow(lid);
+                userWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Gebruiker bestaat niet");
+            }
         }
     }
 }
