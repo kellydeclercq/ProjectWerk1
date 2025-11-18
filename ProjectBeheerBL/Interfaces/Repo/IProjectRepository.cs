fe2289271b5c1da5d27c9b739f4bda6bbaf40cb5
kellydeclercq <204192948+kellydeclercq@users.noreply.github.com>
2025-11-18 13:43:06 +0100

    managers

--- ProjectBeheerBL/Interfaces/Repo/IProjectRepository.cs
+++ ProjectBeheerBL/Interfaces/Repo/IProjectRepository.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectBeheerBL.Domein;
 
 namespace ProjectBeheerBL.Interfaces.Repo
 {
-    public class IProjectRepository
+    public interface IProjectRepository
     {
-
+       
+        
+      
     }
 }
