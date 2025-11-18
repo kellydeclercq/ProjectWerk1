0b5d18077ec905ad1104ac49d49074034adef4b6
manaloc12 <noreply-mail-github>
2025-11-18 10:52:54 +0100

    aanpassingen Adres en Gebruiker

--- ProjectBeheerBL/Domein/Adres.cs
+++ ProjectBeheerBL/Domein/Adres.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectBeheerBL.Domein.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,49 @@ namespace ProjectBeheerBL.Domein
 {
     public class Adres
     {
-        public string Straat { get; set; }
-        public string Huisnummer { get; set; }
-        public int Postcode { get; set; }
-        public string Gemeente { get; set; }
+        private string _straat;
+        public string Straat {
+            get { return _straat; }
+            set {
+                if (string.IsNullOrWhiteSpace(value)) throw new ProjectException("Straatnaam mag niet leeg zijn");
+                var trimmed = value.Trim();
+                _straat = trimmed;
+            }
+        }
+        private string _huisnummer;
+        public string Huisnummer {
+            get { return _huisnummer; }
+            set {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ProjectException("Huisnummer mag niet leeg zijn");
+
+                var trimmed = value.Trim();
+                var first = trimmed.FirstOrDefault();
+                if (!char.IsDigit(first))
+                    throw new ProjectException($"Huisnummer '{value}' moet met een cijfer beginnen");
+
+                _huisnummer = trimmed;
+            }
+        }
+        private int _postcode;
+        public int Postcode {
+            get { return _postcode; }
+            set {
+                if (value >= 1000 && value <= 9999) _postcode = value;
+                else throw new ProjectException($"Postcode {value} niet ok");
+            }
+        }
+        private string _gemeente;
+        public string Gemeente {
+            get { return _gemeente; }
+            set {
+                if (string.IsNullOrWhiteSpace(value)) throw new ProjectException("Woonplaats mag niet leeg zijn");
+                var trimmed = value.Trim();
+                if (trimmed.Length < 2) throw new ProjectException($"Woonplaats '{value}' moet minstens 2 karakters hebben");
+                if (trimmed.Length != value.Length) throw new ProjectException("Woonplaats mag niet met spatie beginnen/eindigen");
+                _gemeente = trimmed;
+            }
+        }
 
         public Adres(string straat, string huisnummer, int postcode, string gemeente)
         {
@@ -19,6 +59,10 @@ namespace ProjectBeheerBL.Domein
             Huisnummer = huisnummer;
             Postcode = postcode;
             Gemeente = gemeente;
+        }
+        public override string ToString()
+        {
+            return $"{Gemeente}, {Straat}, {Huisnummer}, {Postcode}";
         }
     }
 }
