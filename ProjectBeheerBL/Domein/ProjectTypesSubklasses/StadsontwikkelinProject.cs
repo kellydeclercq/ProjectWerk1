8946a727d0906e619b9b700d7e6608728afb8ead
manaloc12 <noreply-mail-github>
2025-11-18 10:17:50 +0100

    Alle types toegevoegd + itnerfaces

--- ProjectBeheerBL/Domein/ProjectTypesSubklasses/StadsontwikkelinProject.cs
+++ ProjectBeheerBL/Domein/ProjectTypesSubklasses/StadsontwikkelinProject.cs
@@ -1,4 +1,5 @@
-﻿using ProjectBeheerBL.Interfaces.projectType;
+﻿using ProjectBeheerBL.Enumeraties;
+using ProjectBeheerBL.Interfaces.projectType;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,12 @@ using System.Threading.Tasks;
 
 namespace ProjectBeheerBL.Domein.ProjectTypesSubklasses
 {
-    public class StadsontwikkelinProject : IStadsontwikkeling
+    public class StadsontwikkelinProject : Project, IStadsontwikkeling
     {
+        public StadsontwikkelinProject(int? id, string projectTitel, string beschrijving, DateTime startDatum, ProjectStatus projectStatus, string wijk, List<byte[]> fotos, List<byte[]> documenten) : base(id, projectTitel, beschrijving, startDatum, projectStatus, wijk, fotos, documenten)
+        {
+        }
+
         public StadsOntwikkeling StadsOntwikkeling { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
