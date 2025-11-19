3e32ccfabde0579e30f84fd6482c7508e05a3c2f
arnolambr <204155001+arnolambr@users.noreply.github.com>
2025-11-19 11:42:16 +0100

    nieuw project methodes voor buttons part one

--- ProjectBeheerWPF_UI/NieuwProject.xaml.cs
+++ ProjectBeheerWPF_UI/NieuwProject.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using ProjectBeheerBL.Beheerder;
+using ProjectBeheerBL.Domein;
 using ProjectBeheerUtils;
 
 namespace ProjectBeheerWPF_UI
@@ -29,6 +30,7 @@ namespace ProjectBeheerWPF_UI
         private BeheerMemoryFactory beheerMemoryFactory;
         private FileDialog fileDialog;
         //lijst met partners bijhouden hier, gewone strings die je kan weergeven in de listbox
+        public List<Partner> partners;
 
         public NieuwProject(ExportManager exportManager, GebruikersManager gebruikersManager, 
             ProjectManager projectManager, BeheerMemoryFactory beheerMemoryFactory)
@@ -55,7 +57,6 @@ namespace ProjectBeheerWPF_UI
             {
                 string bestandsPad = dialog.FileName;
                 MessageBox.Show("Geselecteerd bestand: " + bestandsPad);
-
             }
         }
 
@@ -79,7 +80,42 @@ namespace ProjectBeheerWPF_UI
 
         private void VoegPartnerToeButton_Click(object sender, RoutedEventArgs e)
         {
+            Partner partner = new(NaamPartnerTextBox.Text, EmailPartnerTextBox.Text, 
+                TelefoonPartnerTextBox.Text, PartnerRolTextBox.Text);
+            partners.Add(partner);
+            PartnersListBox.Items.Add(partner.Naam);
+            NaamPartnerTextBox.Clear();
+            EmailPartnerTextBox.Clear();
+            TelefoonPartnerTextBox.Clear();
+            PartnerRolTextBox.Clear();
+        }
 
+        private void GaVerderButtonTab1_Click(object sender, RoutedEventArgs e)
+        {
+            GaVerderButtonTab_Click(sender, e);
+        }
+
+        private void GaVerderButtonTab2_Click(object sender, RoutedEventArgs e)
+        {
+            GaVerderButtonTab_Click(sender, e);
+        }
+
+        private void GaVerderButtonTab3_Click(object sender, RoutedEventArgs e)
+        {
+            GaVerderButtonTab_Click(sender, e);
+        }
+
+        private void GaVerderButtonTab_Click(object sender, RoutedEventArgs e)
+        {
+            if (NieuwProjectTabs.SelectedIndex > NieuwProjectTabs.Items.Count - 1)
+            {
+                NieuwProjectTabs.SelectedIndex += 1;
+            }
+        }
+
+        private void MaakProjectAanButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
